eb337d28b516ca47dbdf13432e4a34da428779d3
harshukla <hshukla@umass.edu>
2019-01-29 13:02:34 -0500

    Sage is cute

--- analysis.py
+++ analysis.py
@@ -6,9 +6,6 @@ from sklearn.linear_model import LinearRegression
 def do(g):
 	# print('-----------------------------------------------------------')
 	# print(g.head())
-	# subject = g.loc[:,'subject'][0]
-	# gender = g.gender[0]
-	# year = int(g.year[0])
 	value = (g.student_count * g.CPI).mean()
 	return value
 
@@ -27,7 +24,7 @@ totalStudentData = pd.read_csv('cleanedData/totalStudentData.csv')
 
 
 #F>M, E>M
-lst = totalStudentData.groupby(['gender','subject','school_code']).apply(lambda frame: do(frame))
+lst = totalStudentData.groupby(['gender','subject','school_code']).apply(lambda frame: frame.CPI.mean() - .5 )
 obj = [lst[i:i+548] for i in range(0, len(lst), 548)]
 print(obj)
 teach = totalTeacherData.groupby('school_code').apply(lambda frame: fuc(frame))
@@ -36,18 +33,26 @@ print(teach.head(teach.shape[0]))
 plt.subplot(221)
 plt.title('Female + ELA')
 plt.scatter(teach,obj[0])
+plt.xlabel('% Male Teachers')
+plt.ylabel('CPI average')
 
 plt.subplot(222)
 plt.title('Female + Math')
 plt.scatter(teach,obj[1])
+plt.xlabel('% Male Teachers')
+plt.ylabel('CPI average')
 
 plt.subplot(223)
 plt.title('Male + ELA')
 plt.scatter(teach,obj[2])
+plt.xlabel('% Male Teachers')
+plt.ylabel('CPI average')
 
 plt.subplot(224)
 plt.title('Male + Math')
 plt.scatter(teach,obj[3])
+plt.xlabel('% Male Teachers')
+plt.ylabel('CPI average')
 
 plt.savefig('result.png')
 
